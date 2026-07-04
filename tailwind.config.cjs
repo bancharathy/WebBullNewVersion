eb14bfe1055209875a138f225a338e9d792a807d
bancharathy <bancharath835@gmail.com>
2026-07-04 16:22:27 +0100

    kkk

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './webbully v2/**/*.{html,js,css}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
